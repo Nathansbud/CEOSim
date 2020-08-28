30958eb558215787bf9d57c76047abfa01143871
Zack Amiton <zamiton@outlook.com>
2020-08-27 18:20:27 -0700

    migrate window to its own file

--- game.ts
+++ game.ts
@@ -1,4 +1,5 @@
 import * as GameEntities from './entity.js'
+import {GameWindow} from './gamewindow.js'
 
 type Gender = 'm' | 'f' | 't' | null //male, female, enby
 class Game {
@@ -53,49 +54,6 @@ class Game {
     public get earnRate() : number {return this._earnRate}
     public get funds() : number {return this._funds}
     public get debt() : number {return this._debt}
-}
-
-class GameWindow {
-    private static manager: GameWindow
-    public formatter: Intl.NumberFormat 
-    
-    private fundText: HTMLSpanElement
-    private earningText: HTMLSpanElement
-    private debtText: HTMLSpanElement
-
-    private workButton: HTMLButtonElement
-    private hireButton: HTMLButtonElement
-
-
-    private constructor() {
-        this.formatter = Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'})
-        
-        this.fundText = document.getElementById('funds') as HTMLSpanElement
-        this.earningText = document.getElementById('earn-rate') as HTMLSpanElement
-        this.debtText = document.getElementById('debt') as HTMLSpanElement
-
-        this.workButton = document.getElementById('work-button') as HTMLButtonElement
-        this.hireButton = document.getElementById('hire-button') as HTMLButtonElement
-        
-    }
-
-    public update(gameObj: Game) {
-        this.updateEarningText(gameObj.earnRate)
-        this.updateFundText(gameObj.funds)
-        this.updateDebtText(gameObj.debt)
-    }
-
-    public mf = (amt: number) => this.formatter.format(amt).slice(0, -3) //drop decimal
-
-    public updateFundText = (amt: number) => this.fundText.textContent = this.mf(amt)
-    public updateEarningText = (amt: number) => this.earningText.textContent = this.mf(amt)
-    public updateDebtText = (amt: number) => this.debtText.textContent = this.mf(amt)
-
-
-    public static getInstance() {
-        if(!GameWindow.manager) GameWindow.manager = new GameWindow()
-        return GameWindow.manager
-    }
 }
 
 const GameManager = Game.getInstance() //Singleton game manager
